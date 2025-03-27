517da25d66cedc1711bbbd180fb318e384b4892a
SoSeDiK <mrsosedik@gmail.com>
2025-03-27 21:14:31 +0200

    Switch to TRenderLib

--- src/main/java/dev/tr7zw/paperdoll/config/ConfigScreenProvider.java
+++ src/main/java/dev/tr7zw/paperdoll/config/ConfigScreenProvider.java
@@ -3,88 +3,103 @@ package dev.tr7zw.paperdoll.config;
 import java.util.ArrayList;
 import java.util.List;
 
-import dev.tr7zw.config.CustomConfigScreen;
+import dev.tr7zw.paperdoll.gui.widget.PaperDollPreviewDummyWidget;
 import dev.tr7zw.paperdoll.PaperDollSettings;
 import dev.tr7zw.paperdoll.PaperDollSettings.DollHeadMode;
 import dev.tr7zw.paperdoll.PaperDollSettings.PaperDollLocation;
 import dev.tr7zw.paperdoll.PaperDollShared;
+import dev.tr7zw.trender.gui.client.AbstractConfigScreen;
+import dev.tr7zw.trender.gui.client.BackgroundPainter;
+import dev.tr7zw.trender.gui.widget.WButton;
+import dev.tr7zw.trender.gui.widget.WGridPanel;
+import dev.tr7zw.trender.gui.widget.WTabPanel;
+import dev.tr7zw.trender.gui.widget.data.Insets;
+import dev.tr7zw.util.ComponentProvider;
 import lombok.experimental.UtilityClass;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
-//#if MC >= 11900
-import net.minecraft.client.OptionInstance;
-//#else
-//$$ import net.minecraft.client.Option;
-//#endif
-//#if MC >= 12000
-import net.minecraft.client.gui.GuiGraphics;
-//#else
-//$$ import com.mojang.blaze3d.vertex.PoseStack;
-//#endif
+import net.minecraft.network.chat.CommonComponents;
 
 @UtilityClass
 public class ConfigScreenProvider {
 
     public static Screen createConfigScreen(Screen parent) {
-        return new CustomConfigScreen(parent, "text.paperdoll.title") {
+        return new ConfigScreen(parent).createScreen();
+    }
 
-            @Override
-            public void initialize() {
-                PaperDollShared inst = PaperDollShared.instance;
-                getOptions().addBig(getOnOffOption("text.paperdoll.enabled", () -> inst.settings.dollEnabled,
-                        (b) -> inst.settings.dollEnabled = b));
-                List<Object> options = new ArrayList<>();
-                options.add(getEnumOption("text.paperdoll.location", PaperDollLocation.class,
-                        () -> inst.settings.location, (loc) -> inst.settings.location = loc));
-                options.add(getEnumOption("text.paperdoll.headMode", DollHeadMode.class,
-                        () -> inst.settings.dollHeadMode, (mode) -> inst.settings.dollHeadMode = mode));
-                options.add(getIntOption("text.paperdoll.xOffset", -100, 100, () -> inst.settings.dollXOffset,
-                        (i) -> inst.settings.dollXOffset = i));
-                options.add(getIntOption("text.paperdoll.yOffset", -100, 100, () -> inst.settings.dollYOffset,
-                        (i) -> inst.settings.dollYOffset = i));
-                options.add(getIntOption("text.paperdoll.size", -12, 40, () -> inst.settings.dollSize,
-                        (i) -> inst.settings.dollSize = i));
-                options.add(getIntOption("text.paperdoll.lookingSides", -80, 80, () -> inst.settings.dollLookingSides,
-                        (i) -> inst.settings.dollLookingSides = i));
-                options.add(getIntOption("text.paperdoll.lookingUpDown", -80, 80, () -> inst.settings.dollLookingUpDown,
-                        (i) -> inst.settings.dollLookingUpDown = i));
-                options.add(getOnOffOption("text.paperdoll.autohide", () -> inst.settings.autoHide,
-                        (b) -> inst.settings.autoHide = b));
-                options.add(getOnOffOption("text.paperdoll.hideInF5", () -> inst.settings.hideInF5,
-                        (b) -> inst.settings.hideInF5 = b));
+    private static class ConfigScreen extends AbstractConfigScreen {
 
-                //#if MC >= 11900
-                getOptions().addSmall(options.toArray(new OptionInstance[0]));
-                //#else
-                //$$getOptions().addSmall(options.toArray(new Option[0]));
-                //#endif
-                // spotless:on
+        public ConfigScreen(Screen previous) {
+            super(ComponentProvider.translatable("text.paperdoll.title"), previous);
 
-            }
+            WGridPanel root = new WGridPanel(8);
+            root.setBackgroundPainter(BackgroundPainter.VANILLA);
+            root.setInsets(Insets.ROOT_PANEL);
+            setRootPanel(root);
 
-            @Override
-            public void save() {
-                PaperDollShared.instance.writeSettings();
-            }
+            WTabPanel wTabPanel = new WTabPanel();
 
-            @Override
-            //#if MC >= 12000
-            public void render(GuiGraphics guiGraphics, int i, int j, float f) {
-                super.render(guiGraphics, i, j, f);
-                //#else
-                //$$ public void render(PoseStack poseStack, int i, int j, float f) {
-                //$$ super.render(poseStack, i, j, f);
-                //#endif
-                // spotless:on
-                PaperDollShared.instance.renderer.render(f);
-            }
+            PaperDollShared inst = PaperDollShared.instance;
+            List<OptionInstance> generalOptions = new ArrayList<>();
+            generalOptions.add(getOnOffOption("text.paperdoll.enabled", () -> inst.settings.dollEnabled,
+                    b -> inst.settings.dollEnabled = b));
+            generalOptions.add(getSplitLine("text.paperdoll.category.doll_settings"));
+            generalOptions.add(getEnumOption("text.paperdoll.location", PaperDollLocation.class, () -> inst.settings.location,
+                    (loc) -> inst.settings.location = loc));
+            generalOptions.add(getEnumOption("text.paperdoll.headMode", DollHeadMode.class, () -> inst.settings.dollHeadMode,
+                    (mode) -> inst.settings.dollHeadMode = mode));
+            generalOptions.add(getIntOption("text.paperdoll.xOffset", -100, 100, () -> inst.settings.dollXOffset,
+                    (i) -> inst.settings.dollXOffset = i));
+            generalOptions.add(getIntOption("text.paperdoll.yOffset", -100, 100, () -> inst.settings.dollYOffset,
+                    (i) -> inst.settings.dollYOffset = i));
+            generalOptions.add(getIntOption("text.paperdoll.size", -12, 40, () -> inst.settings.dollSize,
+                    (i) -> inst.settings.dollSize = i));
+            generalOptions.add(getIntOption("text.paperdoll.lookingSides", -80, 80, () -> inst.settings.dollLookingSides,
+                    (i) -> inst.settings.dollLookingSides = i));
+            generalOptions.add(getIntOption("text.paperdoll.lookingUpDown", -80, 80, () -> inst.settings.dollLookingUpDown,
+                    (i) -> inst.settings.dollLookingUpDown = i));
+            generalOptions.add(getOnOffOption("text.paperdoll.autohide", () -> inst.settings.autoHide,
+                    (b) -> inst.settings.autoHide = b));
+            generalOptions.add(getOnOffOption("text.paperdoll.hideInF5", () -> inst.settings.hideInF5,
+                    (b) -> inst.settings.hideInF5 = b));
 
-            @Override
-            public void reset() {
-                PaperDollShared.instance.settings = new PaperDollSettings();
-                PaperDollShared.instance.writeSettings();
-            }
+            var generalOptionList = createOptionList(generalOptions);
+            generalOptionList.setGap(-1);
+            generalOptionList.setSize(14 * 20, 9 * 20);
+            wTabPanel.add(generalOptionList, b -> b.title(ComponentProvider.translatable("text.paperdoll.tab.general_options")));
 
-        };
+            root.add(wTabPanel, 0, 1);
+
+            WButton doneButton = new WButton(CommonComponents.GUI_DONE);
+            doneButton.setOnClick(() -> {
+                save();
+                Minecraft.getInstance().setScreen(previous);
+            });
+            root.add(doneButton, 0, 27, 6, 2);
+
+            WButton resetButton = new WButton(ComponentProvider.translatable("controls.reset"));
+            resetButton.setOnClick(() -> {
+                reset();
+                root.layout();
+            });
+            root.add(resetButton, 29, 27, 6, 2);
+
+            root.add(new PaperDollPreviewDummyWidget(), 0, 0);
+
+            root.validate(this);
+            root.setHost(this);
+        }
+
+        @Override
+        public void save() {
+            PaperDollShared.instance.writeSettings();
+        }
+
+        @Override
+        public void reset() {
+            PaperDollShared.instance.settings = new PaperDollSettings();
+            PaperDollShared.instance.writeSettings();
+        }
 
     }
 
